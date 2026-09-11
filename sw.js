0bfb527447540b76b8931ba648788313285d2af1
thiagobellagui4 <thiagobellagui4@gmail.com>
2026-09-11 16:22:09 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,19 @@
+const CACHE_NAME = 'macro-meal-v1';
+const ASSETS = [
+  './',
+  './index.html'
+];
 
+// Instala o Service Worker e salva os arquivos no cache do celular
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Intercepta as requisições para carregar o app do cache se estiver sem internet
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
